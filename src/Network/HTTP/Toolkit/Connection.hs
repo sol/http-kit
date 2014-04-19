4c5ee564047b48ec24cef48ba73502b32517ec2a
Simon Hengel <sol@typeful.net>
2014-04-19 11:46:15 +0800

    Add makeConnection

--- src/Network/HTTP/Toolkit/Connection.hs
+++ src/Network/HTTP/Toolkit/Connection.hs
@@ -1,10 +1,12 @@
 module Network.HTTP.Toolkit.Connection (
   Connection(..)
+, makeConnection
 , connectionUnread_
 , connectionReadAtLeast
 ) where
 
-import           Control.Monad (unless)
+import           Control.Monad (join, unless)
+import           Data.IORef
 import           Data.ByteString (ByteString)
 import qualified Data.ByteString as B
 
@@ -12,6 +14,16 @@ data Connection = Connection {
   connectionRead :: IO ByteString
 , connectionUnread :: ByteString -> IO ()
 }
+
+makeConnection :: IO ByteString -> IO Connection
+makeConnection r = do
+  ref <- newIORef []
+  return $ Connection {
+      connectionRead = join $ atomicModifyIORef ref $ \xs -> case xs of
+        y : ys -> (ys, return y)
+        _ -> (xs, r)
+    , connectionUnread = \x -> atomicModifyIORef ref $ \xs -> (x : xs, ())
+    }
 
 connectionUnread_ :: Connection -> ByteString -> IO ()
 connectionUnread_ conn bs = unless (B.null bs) (connectionUnread conn bs)
